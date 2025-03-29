edc887ad944b01e612aee362c85ea3e450fb0eae
niziheng <nzh199266@163.com>
2025-03-29 14:05:07 +0800

    05

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+// 设置路由规则
+const routes = [
+    {
+        path: '/',
+        name: 'main',
+        component: () => import('@/views/Main.vue'),
+    },
+];
+
+// 创建路由对象
+const router = createRouter({
+    // 路由模式
+    history: createWebHashHistory(),
+    routes
+});
+
+export default router;
