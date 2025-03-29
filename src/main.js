fd3cdaf65ceeec4ef897abd7f6a0090d670f9a28
niziheng <nzh199266@163.com>
2025-03-29 14:25:54 +0800

    06

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import "@/assets/less/index.less"
 import router from './router';
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
 app.use(ElementPlus);
 app.use(router);
 app.mount('#app');
+
+// 全局注册element plus icon
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
